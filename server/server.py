01b9309b46dcb7af278b0400c8225bdc1687384a
Aakash Desai <PES1UG24CS006@pesuonline.onmicrosoft.com>
2026-03-09 22:04:47 +0530

    Basic TCP server connectivity test

--- server/server.py
+++ server/server.py
@@ -1,1 +1,19 @@
-print("Distributed Job Queue Server Starting...")
+import socket
+
+HOST = "0.0.0.0"
+PORT = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+print(f"Server listening on port {PORT}")
+
+while True:
+    conn, addr = server.accept()
+    print("Connected from:", addr)
+
+    message = "Hello from Distributed Job Queue Server!\n"
+    conn.send(message.encode())
+
+    conn.close()
